6b7d36440ac15c3642ae7e7b7044fb12283fc1eb
duanlinpeng <duanlinpeng@yunlala.com>
2019-03-14 10:56:26 +0800

    合并

---
+++ src/api/login.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  return request({
+    url: 'api/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/api/user_info',
+    method: 'post',
+    data: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
